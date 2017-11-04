4ad7f011df2df88512ce473f9ca622a35c0f27f7
3nickammo3 <33357844+3nickammo3@users.noreply.github.com>
2017-11-04 14:38:56 -0700

    Card and Part skeletons (#2)

--- src/Part.java
+++ src/Part.java
@@ -1,1 +1,40 @@
 //Part class
+public class Part{
+	
+	public String name;
+	private int level;
+	private int x;
+	private int y;
+	private int h;
+	private int w;
+	private String line;
+	
+	//Constructors
+	
+	Part() {
+		
+	}
+	
+	Part(String name, int level, int x, int y, int h, int w, String line) {
+		this.name = name;
+		this.level = level;
+		this.x = x;
+		this.y = y;
+		this.h = h;
+		this.w = w;
+		this.line = line;
+	}
+	
+	//Getters
+	
+	private String getPartName() {
+		return this.name;
+	}
+	
+	public int getLevel() {
+		return this.level;
+	}
+	public String getLine() {
+		return this.line;
+	}
+}
