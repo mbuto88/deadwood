1ffbd440e9e8e2cc45682d07a7beb5b364af0e81
3nickammo3 <33357844+3nickammo3@users.noreply.github.com>
2017-11-04 14:04:37 -0700

    Card and Part skeletons

--- src/Card.java
+++ src/Card.java
@@ -1,1 +1,34 @@
 //Card Class
+public class Card{
+	
+	private String name;
+	public int number;
+	private String desc;
+	private String img;
+	public Part[] parts;
+	public int budget;
+
+	//Constructors
+	Card(){
+		
+	}
+	
+	Card(String name, int number, String desc, String img, Part[] parts, int budget){
+		this.name = name;
+		this.number = number;
+		this.desc = desc;
+		this.img = img;
+		this.parts = parts;
+		this.budget = budget;
+	}
+	
+	//Getters
+	private void showName() {
+		
+	}
+	
+	//Other methods
+	private void payout() {
+		
+	}
+}
