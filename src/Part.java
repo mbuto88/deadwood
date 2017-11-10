d34d2d6a8bab42a1f99e67e717742771640a6d23
Michael <mbutowicz@gmail.com>
2017-11-10 14:14:06 -0800

    Filled out Part and Card Classes (#5)

--- src/Part.java
+++ src/Part.java
@@ -1,6 +1,6 @@
 //Part class
 public class Part{
-	
+
 	public String name;
 	private int level;
 	private int x;
@@ -8,13 +8,27 @@ public class Part{
 	private int h;
 	private int w;
 	private String line;
-	
+
 	//Constructors
-	
+
 	Part() {
-		
+		this.name = "part";
+		this.level = 1;
+		this.x = 0;
+		this.y = 0;
+		this.h = 0;
+		this.w = 0;
+
+		//https://hipsum.co
+		this.line = "
+			Snackwave gastropub mustache austin bitters tousled viral cliche af retro fashion axe
+			single-origin coffee etsy +1 90's. Yuccie chicharrones taiyaki disrupt.
+			Cliche VHS intelligentsia poke franzen ennui lyft 8-bit lomo.
+			Edison bulb brooklyn mumblecore, vegan echo park jianbing paleo hoodie pok pok thundercats knausgaard small batch.
+			Celiac la croix cronut bespoke.
+			Venmo microdosing whatever pork belly.";
 	}
-	
+
 	Part(String name, int level, int x, int y, int h, int w, String line) {
 		this.name = name;
 		this.level = level;
@@ -24,13 +38,13 @@ public class Part{
 		this.w = w;
 		this.line = line;
 	}
-	
+
 	//Getters
-	
+
 	private String getPartName() {
 		return this.name;
 	}
-	
+
 	public int getLevel() {
 		return this.level;
 	}
