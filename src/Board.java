0dae99ffa2c55139a3b63e475728d47cc28478bb
Michael <mbutowicz@gmail.com>
2017-11-16 23:38:03 -0800

    Updated board class and started working on casting office (#10)

--- src/Board.java
+++ src/Board.java
@@ -1,41 +1,106 @@
 import java.util.*;
 //Board class
-public class Board{
-	private Player[] players;
-    private ArrayList<Room> rooms;
 
-		public void setRooms(ArrayList<Room> rooms) {
-		this.rooms = rooms;
+public class Board{
+	private Room[] rooms;
+  private ArrayList<Player> players;
+	private int daysRemaining;
+
+	//Constructors
+
+	//Default
+	Board(){
+		this.players = new Player[5];
+		this.players = new ArrayList<Player>();
+		setGameLength();
 	}
 
-		//Constructors
-		Board(){
+	//Only room parameters
+	Board(ArrayList<Room> parsedRooms){
+		this.players = new ArrayList<Player>();
+		this.rooms = new Room[rooms.size()];
+		setRooms(parsedRooms);
+		setGameLength();
+	}
 
+	//Both room and player parameters
+	Board(ArrayList<Room> parsedRooms, ArrayList<Player> parsedPlayers){
+		this.players = new ArrayList<Player>();
+		this.rooms = new Room[rooms.size()];
+		populate(parsedRooms);
+		populate(players);
+		setGameLength();
+	}
+
+	//Helper methods for Constructors
+	private void populate(ArrayList<Room> parsedRooms) {
+		int i = 0;
+		while(i < parsedRooms.size()){
+			this.rooms[i] = parsedRooms.get(i);
+			i++;
 		}
+	}
 
-		Board(ArrayList<Room> rooms){
+	private void populate(ArrayList<Player> parsedPlayers) {
+		int i = 0;
+		while(i < parsedRooms.size()){
+			this.players.add(parsedRooms.get(i));
+			i++;
 		}
+	}
 
-		//Getters
-		public void renderBoard(){
+	//Determines length of gameplay in game days
+	private void setGameLength(int players){
+		if(players <= 3){
+			this.daysRemaining = 3;
+		} else {
+			this.daysRemaining = 4;
 		}
+	}
 
-		public void showScore(){
+
+	//Getters
+	/* GUI Related method
+	public void renderBoard(){
+	} */
+
+	//Outputs score of all players
+	public void showScore(){
+		int i = 0;
+		while(i < players.size()){
+			system.out.println("Cr/Fame: " +  players.get(i).getFame()
+																+ " $" + players.get(i).getFame());
+			i++;
 		}
+	}
 
-		//Other Methods
-		public int rollDice(){
-         Random rand = new Random();
-         int val = rand.nextInt(6) + 1;
-         System.out.println("You rolled a " + val);
-         return val;
-		}
+	//Outputs score of specified player
+	public void showScore(Player player){
+			system.out.print("Cr/Fame: " +  player.getFame()
+																+ " Money: $" + players.get(i).getFame());
+	}
 
-		private int calcScore(Player p){
-      return p.getFame() + p.getMoney() + p.getRank();
-		}
+	public int getDaysLeft(){
+		return this.daysRemaining;
+	}
 
-		private void nextDay(){
+	//Other Methods
+	public int rollDice(){
+		int result;
+		Random roll = new Random();
+	  result = roll.nextInt(6);
 
-		}
+		System.out.println("Roll was: "+ result);
+		return result;
+	}
+
+	private int calcScore(Player player){
+		//dollars + fame + (5* rank)
+		int score = player.getFame() + player.getMoney() + (5 * player.getRank());
+		return score;
+	}
+
+	private void nextDay(){
+		this.daysRemaining--;
+	}
 }
